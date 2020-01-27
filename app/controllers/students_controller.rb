bfa879129fd74f11924088feab6c2392e90ee485
Hanna Lee <mani21na@gmail.com>
2020-01-27 02:40:19 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    session[:form_params] = params.inspect
-    redirect_to new_student_path
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
   end
 end
